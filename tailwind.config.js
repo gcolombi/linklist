9c828d86c460e6c24994b1ae8e1984edc2e26cdf
gcolombi <gg_1605@hotmail.fr>
2023-04-03 22:48:51 -0400

    add .editorconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
+    ],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
