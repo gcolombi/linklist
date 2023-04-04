d1e72e7b6b28abd0b904a8195e78daa8ee64dea1
gcolombi <gg_1605@hotmail.fr>
2023-04-04 00:03:50 -0400

    update index page - header WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: [
+            'images.unsplash.com',
+            'github.com/devicons/devicon/blob/master/icons/',
+            'pbs.twimg.com'
+        ]
+    }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
-// import Image from 'next/image'
+import Image from 'next/image';
+import data from '../data.json';
 
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="flex flex-col items-center justify-center mt-16">
+            <Image
+                className="rounded-full"
+                alt={data.name}
+                src={data.avatar}
+                width={96}
+                height={96}
+                priority
+            />
+            <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+        </div>
     )
 }
