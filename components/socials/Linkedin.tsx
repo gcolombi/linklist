2ef503d1f2f25c4781a297cf2376bf124835edfe
gcolombi <gg_1605@hotmail.fr>
2023-04-05 23:59:31 -0400

    add Instagram logo

--- components/socials/Linkedin.tsx
+++
@@ -1,5 +0,0 @@
-export default function Linkedin() {
-    return (
-        <></>
-    );
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Instagram from '@/components/socials/Instagram';
+import Twitter from '@/components/socials/Twitter';
 import Image from 'next/image';
 import data from '../data.json';
 
@@ -53,6 +55,10 @@ export default function Home() {
                     {data.links.map((link) => (
                         <LinkCard key={link.href} {...link} />
                     ))}
+                    <div className="bg-red-600">
+                        <Twitter />
+                        <Instagram />
+                    </div>
                 </div>
             </div>
         </div>
