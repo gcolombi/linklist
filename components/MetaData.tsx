5bf6500d0acd92bbc230c7fa752b39c75479cd65
gcolombi <gg_1605@hotmail.fr>
2023-04-06 01:19:07 -0400

    add MetaData component - WIP

---
+++ components/MetaData.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function MetaData() {
+    return (
+        <Head>
+            <meta charSet="utf-8" />
+        	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+    );
+}
