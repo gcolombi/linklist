e8e7b57ee0e5fa4d9e5dc995c5d95b54dce5048f
gcolombi <gg_1605@hotmail.fr>
2023-04-06 21:16:12 -0400

    update MetaData - add all meta & set default props value

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -1,21 +1,32 @@
 import Head from 'next/head';
 import useWindowLocation from '@/hooks/useWindowLocation';
 
-export default function MetaData() {
+export default function MetaData({
+    title = 'Linktree',
+    description = 'Linktree. Make your link do more.',
+    image = `${process.env.NEXT_PUBLIC_BASE_URL}/static/example.jpg`,
+    type = 'profile'
+}: {
+    title?: string;
+    description?: string;
+    image?: string;
+    type?: string;
+}) {
     const { currentURL } = useWindowLocation();
+
     return (
         <Head>
             <meta charSet="utf-8" />
         	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>{''}</title>
-            <meta name="description" content={''} />
-            <meta property="og:title" content={''} />
-            <meta property="og:description" content={''} />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
             <meta property="og:url" content={currentURL} />
             <meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
-            <meta property="og:type" content={''} />
-            <meta property="og:image" content={''} />
+            <meta property="og:type" content={type} />
+            <meta property="og:image" content={image} />
             <meta name="robots" content="follow, index" />
             <link rel="canonical" href={currentURL} />
         </Head>
