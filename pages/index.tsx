3424ab45fb1c3c76c1f22c8ac1a699906e512a03
gcolombi <gg_1605@hotmail.fr>
2023-04-07 17:29:12 -0400

    update ListItem style

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,28 +12,29 @@ function ListItem({
     image?: string;
 }) {
     return (
-        <a
-            href={href}
-            className="p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3"
-        >
-            <div className="flex items-center w-full">
-                <div className="w-10 h-10">
+        <li className="relative w-full group [&:not(:last-child)]:mb-4 hover:scale-105 transition-all">
+            <a
+                href={href}
+                className="relative block py-4 px-16 w-full rounded bg-gray-100"
+            >
+                <div className="flex items-center w-full">
                     {image && (
-                        <Image
-                            className="rounded-sm"
-                            alt={title}
-                            src={image}
-                            width={40}
-                            height={40}
-                        />
+                        <div className="absolute top-1/2 left-1 -translate-y-1/2 w-12 h-12">
+                            <Image
+                                className="rounded"
+                                alt={title}
+                                src={image}
+                                width={48}
+                                height={48}
+                            />
+                        </div>
                     )}
+                    <h2 className="flex-grow font-semibold text-gray-700 text-center">{title}</h2>
                 </div>
-                <h2 className="flex-grow font-semibold text-gray-700 text-center">{title}</h2>
-                <div className="w-10 h-10">
-                    <button className="w-full h-full"></button>
-                </div>
-            </div>
-        </a>
+            </a>
+            <button className="absolute top-1/2 right-2 -translate-y-1/2 w-10 h-10 rounded-full hover:bg-black/20 group-hover:opacity-100 opacity-0 transition-all">
+            </button>
+        </li>
     )
 }
 
@@ -54,14 +55,14 @@ export default function Home() {
                                 height={96}
                                 priority
                             />
-                            <h1 className="font-bold mt-4 mb-8 text-xl text-white">@{data.name}</h1>
+                            <h1 className="font-bold mt-4 text-xl text-white">@{data.name}</h1>
                         </div>
 
-                        <div className="flex flex-col items-center">
+                        <ul className="flex flex-col items-center mt-8">
                             {data.links.map((link) => (
                                 <ListItem key={link.href} {...link} />
                             ))}
-                        </div>
+                        </ul>
 
                     </div>
                 </div>
