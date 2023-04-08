590d647b6ec28f9385361e2ba848f12caf574e16
gcolombi <gg_1605@hotmail.fr>
2023-04-08 17:59:18 -0400

    removed dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,8 @@
-import Image from 'next/image';
 import data from '../data.json';
 import MetaData from '@/components/MetaData';
-import Share from '@/components/icons/Share';
 import Icon from '@/components/icons/socialMedias';
-import React from 'react';
 import LinksListHeader from '@/components/LinksListHeader';
 import LinksList from '@/components/LinksList';
-
-type Item = {
-    title: string;
-    href: string;
-    image?: string;
-}
-
-function ListItem({
-    title,
-    href,
-    image
-}: Item ) {
-    return (
-        <li className="relative group [&:not(:last-child)]:mb-4 hover:scale-105 transition-all rounded bg-white/80 text-black">
-            <a
-                href={href}
-                className="relative block py-4 px-16 text-center"
-            >
-                {image &&
-                    <div className="absolute top-1/2 left-1 -translate-y-1/2 w-12 h-12">
-                        <Image
-                            className="rounded"
-                            alt={title}
-                            src={image}
-                            width={48}
-                            height={48}
-                        />
-                    </div>
-                }
-                <h2 className="font-medium">{title}</h2>
-            </a>
-            <button className="flex justify-center items-center absolute top-1/2 right-2 -translate-y-1/2 w-10 h-10 rounded-full hover:bg-black/20 group-hover:opacity-100 opacity-0 transition-all">
-                <Share />
-            </button>
-        </li>
-    )
-}
 
 export default function Home() {
     return (
