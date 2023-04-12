6b5dd831bb4fdbc7fddb696e05571d37faa06fea
gcolombi <gg_1605@hotmail.fr>
2023-04-12 19:47:31 -0400

    add Chevron component

---
+++ components/icons/Chevron.tsx
@@ -0,0 +1,7 @@
+export default function Chevron() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" viewBox="0 0 16 16" width="16" height="16">
+            <path d="M4.70714 1.29297L5.06069 1.64652L11.0607 7.64652V8.35363L5.06069 14.3536L4.70714 14.7072L4.00003 14.0001L4.35358 13.6465L10 8.00008L4.35358 2.35363L4.00003 2.00008L4.70714 1.29297Z" fill="black"/>
+        </svg>
+    );
+};
