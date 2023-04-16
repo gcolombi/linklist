f5786d6c66ab8c51d31c4be92135f1f9c6f6fc8a
gcolombi <gg_1605@hotmail.fr>
2023-04-16 10:44:05 -0400

    update TopBar - style

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -20,10 +20,10 @@ export default function TopBar() {
     return (
         <header
             className={classNames(
-                'fixed top-0 left-0 right-0 z-[1000] w-[calc(100%-24px)] py-2 sm:py-3 px-3 grid items-center grid-cols-[1fr_auto_1fr] max-w-[788px] my-2 sm:my-3 mx-auto rounded-[72px] border-solid border transition-colors duration-300',
+                'fixed top-0 left-0 right-0 z-[1000] w-[calc(100%-24px)] py-2 sm:py-3 px-3 grid items-center grid-cols-[1fr_auto_1fr] max-w-[788px] my-2 sm:my-3 mx-auto rounded-[72px] border-solid border transition-[background]',
                 {
                     'bg-transparent border-transparent': !sticky,
-                    'bg-white/50 border-black/10 backdrop-blur-md': sticky
+                    'bg-white/50 border-[rgb(235, 238, 241)] backdrop-blur-md': sticky
                 }
             )}
             ref={(el) => setRef(el)}
