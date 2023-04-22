1a9c84a3d265da3097ddd3f3f33922e6ce24859c
gcolombi <gg_1605@hotmail.fr>
2023-04-22 14:15:20 -0400

    add TranslateIn component & update LinkListHeader

--- components/LinksListHeader.tsx
+++ components/LinksListHeader.tsx
@@ -1,5 +1,7 @@
 import { ListHeader } from '@/lib/types';
 import Image from 'next/image';
+import FadeIn from './gsap/FadeIn';
+import TranslateIn from './gsap/TranslateIn';
 
 export default function LinksListHeader({
     avatar,
@@ -9,7 +11,10 @@ export default function LinksListHeader({
     return (
         <div className="flex flex-col items-center">
             {avatar &&
-                <div>
+                <FadeIn
+                    delay={0.5}
+                    ease="sine.in"
+                >
                     <Image
                         className="w-24 h-24 object-contain rounded-full"
                         alt={name}
@@ -18,14 +23,28 @@ export default function LinksListHeader({
                         height={96}
                         priority
                     />
-                </div>
+                </FadeIn>
             }
-            <div className="mt-4">
-                <h1 className="font-bold text-xl text-white">@{name}</h1>
+            <div className="mt-4 overflow-hidden">
+                <TranslateIn
+                    fade={false}
+                    delay={0.7}
+                    y="100%"
+                >
+                    <h1 className="font-bold text-xl text-white">@{name}</h1>
+                </TranslateIn>
             </div>
             {content &&
-                <div className="mt-1 px-10 text-sm text-white text-center sm:text-base">
-                    <p>{content}</p>
+                <div className="overflow-hidden">
+                    <TranslateIn
+                        fade={false}
+                        delay={0.9}
+                        y="100%"
+                    >
+                        <div className="mt-1 px-10 text-sm text-white text-center sm:text-base">
+                            <p>{content}</p>
+                        </div>
+                    </TranslateIn>
                 </div>
             }
         </div>
---
+++ components/gsap/TranslateIn.tsx
@@ -0,0 +1,60 @@
+import { ReactNode } from 'react';
+import Animate from './Animate';
+
+type Translate = {
+    children: ReactNode,
+    fade?: boolean;
+    durationIn?: number,
+    delay?: number,
+    ease?: string;
+    x?: string;
+    y?: string;
+    xTo?: number;
+    yTo?: number;
+    watch?: boolean,
+    start?: string,
+    end?: string,
+    scrub?: boolean,
+    markers?: boolean
+}
+
+export default function TranslateIn({
+    children,
+    fade = true,
+    durationIn = 0.5,
+    delay = 0,
+    ease = 'power4.out',
+    x = '0px',
+    y = '0px',
+    xTo = 0,
+    yTo = 0,
+    watch,
+    start = 'top bottom',
+    end = 'bottom top',
+    scrub = false,
+    markers
+}: Translate ) {
+    return (
+        <Animate
+            durationIn={durationIn}
+            delay={delay}
+            from={{
+                opacity: fade ? 0 : 1,
+                transform: `translate(${x}, ${y})`
+            }}
+            to={{
+                ease,
+                opacity: 1,
+                x: xTo,
+                y: yTo
+            }}
+            watch={watch}
+            start={start}
+            end={end}
+            scrub={scrub}
+            markers={markers}
+        >
+            {children}
+        </Animate>
+    );
+}
