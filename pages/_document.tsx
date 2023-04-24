7c55c117dfbe99e5d94fb110b070a2d5e2d12bb3
gcolombi <gg_1605@hotmail.fr>
2023-04-24 00:54:32 -0400

    update _document - typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-            <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
                 <link rel="manifest" href="/static/favicons/site.webmanifest" />
